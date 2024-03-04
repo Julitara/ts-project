60aafce30ebe326b1a13840803913083ab8e66d8
Юлия <tarasovayuliya00@gmail.com>
2024-03-03 19:14:18 -0500

    add config

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,31 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
+
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  const { isDev } = options;
+  const cssLoaders = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+            localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]',
+          },
+        },
+      },
+      'sass-loader',
+    ],
+  };
+
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  };
+
+  return [cssLoaders, typescriptLoader];
+}
