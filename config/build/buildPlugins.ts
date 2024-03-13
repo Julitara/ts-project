a62cfc753ee487225a6b0817b687409c22f814c6
Юлия <tarasovayuliya00@gmail.com>
2024-03-13 15:44:08 -0400

    chanche webpack plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,7 +5,8 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+
+    const plugins = [
         new webpack.ProgressPlugin(),
         new HtmlWebpackPlugin({ template: paths.html }),
         new MiniCssExtractPlugin({
@@ -15,9 +16,14 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
         }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
+    ]
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin())
+        plugins.push(new BundleAnalyzerPlugin({
             openAnalyzer: false
-        })
-    ];
+        }))
+    }
+
+    return plugins;
 }
