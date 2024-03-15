7f4d883990473506ec5bd5d0a281cb4c015f9353
Юлия <tarasovayuliya00@gmail.com>
2024-03-15 16:06:23 -0400

    fix sidebar test

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,18 +1,17 @@
 /* eslint-disable i18next/no-literal-string */
 import { screen, fireEvent } from '@testing-library/react';
-import { renderWithTranslation } from 
-    'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from 'widgets/Sidebar';
 
 describe('Sidebar', () => {
 
     test('sidebar test: render', () => {
-        renderWithTranslation(<Sidebar/>);
+        componentRender(<Sidebar/>);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('sidebar test: toggle', () => {
-        renderWithTranslation(<Sidebar/>);
+        componentRender(<Sidebar/>);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
