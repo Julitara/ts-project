42af942b23085e40f6e13a87f44471c39986dd70
Юлия <tarasovayuliya00@gmail.com>
2024-07-13 11:08:32 -0400

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
+axios.defaults.baseURL = 'https://frontend-ts-server-jscsvb1ys-julitaras-projects.vercel.app';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.headers.common['Authorization'] = 'Bearer YOUR_TOKEN'; // Если требуется авторизация
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = 
+'https://tarasova-frontend-project.netlify.app';
+
 export const $api = axios.create({
     baseURL: __API__,
 });
